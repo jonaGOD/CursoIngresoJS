9f872934dbd08a71cf9cd0fb4ffbe76065c29d72
alumno <alumno>
2019-02-05 22:17:39 -0300

    Ejercicios 05.02 Parte8

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad;
+edad = document.getElementById("edad").value;
+if (edad<18) {
+    alert("Es menor de edad");
+    } else {
+    alert("Es mayor de edad")
+    }
 }//FIN DE LA FUNCIÓN
