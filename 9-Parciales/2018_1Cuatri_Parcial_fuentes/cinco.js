2f9561646b593f3e150dd3b04e97ac0288d03220
Rodriguez <jonathan.rodriguez@externos-ar.cencosud.com>
2019-02-13 18:03:17 -0300

    13-feb2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,30 @@
 function mostrar()
 {
+    var planeta
+    planeta = prompt("Ingrese un planeta");
 
+    switch(planeta) {
+        case "tierra":
+        alert("Acá vivimos");
+        break;
+        case "mercurio":
+        case "venus":
+        alert("Acá hace más calor");
+        break;
+        case "marte":
+        case "jupiter":
+        case "ceres":
+        case "saturno":
+        case "urano":
+        case "neptuno":
+        case "plutón":
+        case "huamea":
+        case "makemake":
+        case "eris":
+        alert("Acá hace más frío");
+        break;
+        default:
+        alert("No es un planeta válido");
+        break;
+    }
 }
