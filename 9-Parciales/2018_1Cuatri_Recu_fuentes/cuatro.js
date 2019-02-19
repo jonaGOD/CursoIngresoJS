5b3028e62c037ecd4a5f3f4eab5b13b2ee656853
jonaGOD <jonarodriguez91@gmail.com>
2019-02-19 01:16:15 -0300

    18-02 en casa2

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -3,36 +3,51 @@ function mostrar()
     var libros
     var precioLibros
     var pagaTarjeta
+    var totalDesc
     var totalPaga
 
     libros = prompt("¿Cuantos libros va a llevar?");
     precioLibros = prompt("Ingrese el valor total de los libros");
     pagaTarjeta = prompt("¿Abona con tarjeta? Si o No");
 
-    if (pagaTarjeta == "no" && (libros < 2 && precioLibros < 2000)) {
-        totalPaga = parseInt(precioLibros);
-        alert("Debe abonar $"+precioLibros);
-
-    } else {
-        if (pagaTarjeta == "si" && (libros < 2 && precioLibros < 2000)) {
-            totalPaga = parseInt(precioLibros) + (parseInt(precioLibros) * 0.1);
+    switch (pagaTarjeta) {
+        case "si":
+        if (libros > 2 && precioLibros > 2000) {
+            totalDesc = parseInt(precioLibros) - (parseInt(precioLibros) * 0.1);
+            totalPaga = parseInt(totalDesc) - (parseInt(totalDesc) * 0.15);
+            totalPaga = parseInt(totalPaga) + (parseInt(totalPaga) * 0.1);
             alert("Debe abonar $"+totalPaga);
-
-        } else {
-            if (pagaTarjeta == "no" && (libros > 2 && precioLibros < 2000)) {
-                totalPaga = parseInt(precioLibros) - (parseInt(precioLibros) * 0.1);
-                alert("Debe abonar $"+totalPaga);
+            
+            } else {
+                if (libros > 2) {
+                    totalPaga = parseInt(precioLibros) + (parseInt(precioLibros) * 0.1);
+                    alert("Debe abonar $"+totalPaga);
                 
                 } else {
-                    if (pagaTarjeta == "si" && (libros > 2 && precioLibros < 2000)) {
-                        totalPaga = parseInt(precioLibros) - (parseInt(precioLibros) * 0.1);
-                        totalPaga = parseInt(totalPaga) + ((parseInt(precioLibros) * 0.1));
-                        alert("Debe abonar $"+totalPaga);
-                    
-                    } else {
-                        if ()
+                    if (libros < 2) {
+                        totalPaga = parseInt(precioLibros) + (parseInt(precioLibros) * 0.1);
+                        alert("Debe abonar $"+totalPaga);                        
                     }
+                }
             }
-        }
+        break;
+        default:
+        if (libros > 2 && precioLibros > 2000) {
+            totalDesc = parseInt(precioLibros) - (parseInt(precioLibros) * 0.1);
+            totalPaga = parseInt(totalDesc) - (parseInt(totalDesc) * 0.15);
+            alert("Debe abonar $"+totalPaga);
+
+            } else {
+                if (libros > 2) {
+                    totalPaga = parseInt(precioLibros) - (parseInt(precioLibros) * 0.1);
+                    alert("Debe abonar $"+totalPaga);
+
+                    } else {
+                        if (libros < 2) {
+                        alert("Debe abonar $"+precioLibros);      
+                        }
+                    }
+                }
+        break;
     }
 }
