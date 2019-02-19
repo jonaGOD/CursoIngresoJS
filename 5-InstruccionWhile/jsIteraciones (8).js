f0c6287f392bfa366d9c4ef71021ce642b2b7eac
jonaGOD <jonarodriguez91@gmail.com>
2019-02-19 14:57:05 -0300

    19.02 work3

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,11 +1,19 @@
 function mostrar()
 {
-
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
-	
 	var respuesta='si';
+	var numero;
+	var acumulador;
+
+	while (respesta == "si") {
+		numero = prompt("Ingrese el número");
+		if (numero < 0) {
+			acumulador = parseInt(numero) + parseInt()
+		}
+
+	}
 
 
 document.getElementById('suma').value=positivo;
